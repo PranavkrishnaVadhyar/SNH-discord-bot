ee02e9c7407181167de3ae77e82bb945e9c4248a
B Pranavkrishna Vadhyar <96326149+PranavkrishnaVadhyar@users.noreply.github.com>
2022-11-06 23:27:42 +0530

    Add files via upload
    commit 2

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import discord
 import os
 import requests
 import json
+import random
+
 
 client = discord.Client(intents=discord.Intents.all())
+
+data = ['depressed','sad','down','angry','desperate','lonely','unhappy']
+
+responese = ['Heyy you are the best','Dont worry. Everything will be ok ','You are the best developer I know','Just think of the good memories you had','Just think about the good things in your life']
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -25,5 +31,10 @@ async def on_message(message):
         quote = get_quote()
         await message.channel.send(quote)
         
+        
+        
+    if any(word in message.content for word in data):
+            
+            await message.channel.send(random.choice(responese))
 
 client.run(os.getenv('TOKEN'))
